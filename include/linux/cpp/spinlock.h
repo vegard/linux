3316297d7d8121b54308db69c672d9f02f571720
Vegard Nossum <vegard.nossum@oracle.com>
2015-09-11 16:51:25 +0200

    cpp: Spinlock wrapper class + helper classes

---
+++ include/linux/cpp/spinlock.h
@@ -0,0 +1,106 @@
+#ifndef __LINUX_CPP_SPINLOCK_H
+#define __LINUX_CPP_SPINLOCK_H
+
+extern "C" {
+#include <linux/spinlock.h>
+}
+
+class Spinlock {
+public:
+	spinlock_t spinlock;
+
+	Spinlock()
+	{
+		spin_lock_init(&spinlock);
+	}
+
+	~Spinlock()
+	{
+	}
+
+	bool is_locked() const
+	{
+		return spin_is_locked(&spinlock);
+	}
+
+	void lock()
+	{
+		spin_lock(&spinlock);
+	}
+
+	void unlock()
+	{
+		spin_unlock(&spinlock);
+	}
+
+	void lock_irq()
+	{
+		spin_lock_irq(&spinlock);
+	}
+
+	void unlock_irq()
+	{
+		spin_unlock_irq(&spinlock);
+	}
+
+	void lock_irqsave(unsigned long &flags)
+	{
+		spin_lock_irqsave(&spinlock, flags);
+	}
+
+	void unlock_irqrestore(unsigned long &flags)
+	{
+		spin_unlock_irqrestore(&spinlock, flags);
+	}
+};
+
+class SpinlockLocker {
+public:
+	Spinlock &spinlock;
+
+	SpinlockLocker(Spinlock &spinlock):
+		spinlock(spinlock)
+	{
+		spinlock.lock();
+	}
+
+	~SpinlockLocker()
+	{
+		spinlock.unlock();
+	}
+};
+
+class SpinlockIRQLocker {
+public:
+	Spinlock &spinlock;
+
+	SpinlockIRQLocker(Spinlock &spinlock):
+		spinlock(spinlock)
+	{
+		spinlock.lock_irq();
+	}
+
+	~SpinlockIRQLocker()
+	{
+		spinlock.unlock_irq();
+	}
+};
+
+class SpinlockIRQSaveLocker {
+public:
+	Spinlock &spinlock;
+	unsigned long flags;
+
+	SpinlockIRQSaveLocker(Spinlock &spinlock):
+		spinlock(spinlock)
+	{
+		spinlock.lock_irqsave(flags);
+	}
+
+	~SpinlockIRQSaveLocker()
+	{
+		spinlock.unlock_irqrestore(flags);
+	}
+};
+
+#endif
