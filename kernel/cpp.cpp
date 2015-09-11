20dc397ad4bbc01d05bd6c8b06dc11a3ae0a866c
Vegard Nossum <vegard.nossum@oracle.com>
2015-09-11 11:59:51 +0200

    [tmp] Sample C++ code
    This sample C++ code only exists to check that C++ code compiles
    correctly.

---
+++ kernel/cpp.cpp
@@ -0,0 +1,54 @@
+extern "C" {
+#include <linux/init.h>
+#include <linux/printk.h>
+}
+
+#include <linux/cpp/spinlock.h>
+
+struct error {
+	int x;
+
+	error(int x): x(x) {}
+};
+
+class KernelClass {
+public:
+	int value;
+	Spinlock lock;
+
+	KernelClass():
+		value(100)
+	{
+	}
+
+	void foo()
+	{
+		//throw error(123);
+		throw 10;
+	}
+};
+
+static int __init cpp_init()
+{
+	KernelClass kernel_class;
+
+	{
+		SpinlockIRQSaveLocker locker(kernel_class.lock);
+
+		printk(KERN_ERR "cpp_init: %u, locked? %s\n",
+			kernel_class.value,
+			kernel_class.lock.is_locked() ? "yes" : "no");
+
+		try {
+			kernel_class.foo();
+		} catch (const error &e) {
+			printk(KERN_ERR "cpp_init(): caught error with value %d\n", e.x);
+		} catch (const int x) {
+			printk(KERN_ERR "cpp_init(): caught int with value %d\n", x);
+		}
+	}
+
+	return 0;
+}
+
+late_initcall(cpp_init)
